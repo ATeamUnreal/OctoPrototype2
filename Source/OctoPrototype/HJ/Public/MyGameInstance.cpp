a33fe3833991abb68f0e720efedc7a5eb890540c
dokilove <dokilove2@gmail.com>
2019-10-06 00:26:29 +0900

    게임 입장과 네트워크 에러 겸용 씬 작동하게끔 처리

--- Source/OctoPrototype/HJ/Public/MyGameInstance.cpp
+++ Source/OctoPrototype/HJ/Public/MyGameInstance.cpp
@@ -4,13 +4,10 @@
 #include "MyGameInstance.h"
 #include "SocketSubsystem.h"
 #include <IPAddress.h>
-#include "Engine/World.h"
-#include "GameFramework/PlayerController.h"
 
 void UMyGameInstance::Init()
 {
 	Super::Init();
-	GetEngine()->OnNetworkFailure().AddUObject(this, &UMyGameInstance::HandleNetworkFailure);
 
 }
 
@@ -25,29 +22,5 @@ FString UMyGameInstance::GetMyIpAddress()
 	}
 
 	return IpAddr;
-}
-
-void UMyGameInstance::HandleNetworkFailure(UWorld * World, UNetDriver * NetDriver, ENetworkFailure::Type FailureType, const FString & ErrorString)
-{
-	UE_LOG(LogClass, Warning, TEXT("ERRRROR String : %s"), *ErrorString);
-	//World->NextURL = FString("/Game/Maps/NetworkError");
-	
-	
-	APlayerController* PC = GetFirstLocalPlayerController();
-	if (PC && PC->IsValidLowLevel())
-	{
-		UE_LOG(LogClass, Warning, TEXT("Travel!!"));
-		PC->ClientTravel("/Game/Maps/NetworkError", ETravelType::TRAVEL_Absolute);	
-	}
-
-}
-
-void UMyGameInstance::ClientTravel(FString path)
-{
-	APlayerController* PC = GetFirstLocalPlayerController();
-	if (PC && PC->IsValidLowLevel())
-	{
-		PC->ClientTravel(path, ETravelType::TRAVEL_Absolute);
-	}
 }
 
